a3e950684ba0e750e21d787debd1a2d23e0df42e
Abdulla Ameen <abdulla.atwork@gmail.com>
2025-09-24 22:27:10 +0530

    v1.5.1

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
 const words =
   "The wind whispered softly through the trees as the early morning light began to spill across the quiet meadow where birds sang cheerfully and small animals darted between the tall grasses while a young writer sat beneath an old oak tree scribbling ideas into a worn notebook dreaming of distant lands and untold stories that lived only in the corners of her mind the pages filled quickly with thoughts of brave heroes ancient prophecies hidden kingdoms and magical creatures that soared through endless skies or lurked in deep enchanted forests she imagined a world where time moved differently where the stars told secrets and the moon watched over all who dared to dream she wrote of friendships forged in fire of battles fought with honor and of love found in the most unexpected places her hand moved faster than her thoughts trying to capture every fleeting image and every whisper of inspiration that came with the rising sun as the shadows shifted and the world around her came alive with golden light she smiled knowing that within those simple words lay entire worlds waiting to be explored by anyone willing to read and believe in the impossible and she promised herself that no matter how hard the journey she would keep writing because her stories deserved to be told and her imagination was a gift she would never let go of no matter how many doubts tried to silence her voice".split(
     " "
-  );
+  ); // ✅
 
 let wordsCount = words.length;
-const gameTime = 3 * 1000; // 1 minute
+const gameTime = 30 * 1000; // 30 seconds
 window.timer = null;
 window.gameStart = null;
+window.pauseTime = 0;
+// ✅
 
 function addClass(el, name) {
   el.className += " " + name;
 }
 function removeClass(el, name) {
   el.className = el.className.replace(name, "");
-}
+} // ✅
 
 function randomWord() {
-  const randonIndex = Math.floor(Math.random() * wordsCount);
-  return words[randonIndex];
-}
+  const randonIndex = Math.ceil(Math.random() * wordsCount);
+  return words[randonIndex - 1];
+}// ✅
 
 function formatCode(word) {
   return `<div class="word"><span class="letter">${word
     .split("")
     .join('</span><span class="letter">')}</span></div>`;
-}
+} // ✅
 
 function newGame() {
   document.querySelector("#words").innerHTML = "";
 
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 200; i++) {
     document.querySelector("#words").innerHTML += formatCode(randomWord());
   }
   addClass(document.querySelector(".word"), "current");
   addClass(document.querySelector(".letter"), "current");
-}
+  document.getElementById('info').innerHTML = (gameTime / 1000) + '';
+  window.timer = null;
+} // ✅
 
 function getWpm(){
   const words = [...document.querySelectorAll(".word")];
   const lastTypedWord = document.querySelector(".word.current");
-  const lastTypedWordIndex = words.indexOf(lastTypedWord);
+  const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
   const TypedWords = words.slice(0, lastTypedWordIndex);
   const correctWords = TypedWords.filter(word => {
     const letters = [...word.children];
@@ -47,47 +51,52 @@ function getWpm(){
     const correctLetters = letters.filter(letter => letter.className.includes("correct"));
 
     return incorrectLetters.length === 0 && correctLetters.length > 0;
-  })
-  return 23;
-}
+  });
+  return correctWords.length / gameTime * 60000;
+} // ✅
 
 function gameOver() {
   clearInterval(window.timer);
   document.querySelector("#game").classList.add("over");
-  document.getElementById('wpm').innerHTML = getWpm();
-}
+  const result = getWpm();
+  document.getElementById('wpm').innerHTML = result;
+} // ✅
 
-document.querySelector("#game").addEventListener("keypress", (e) => {
+document.querySelector("#game").addEventListener("keyup", e => {
   const key = e.key;
   const currentWord = document.querySelector(".word.current");
   const currentLetter = document.querySelector(".letter.current");
-  const expected = currentLetter?.innerHTML || " ";
-  const isLetter = key.length === 1 && key !== " ";
+  const expected = currentLetter?.innerHTML || ' ';
+  const isLetter = key.length === 1 && key !== ' ';
   const isSpace = key === " ";
   const isBackSpace = key === "Backspace";
   const isFirstLetter = currentLetter === currentWord.firstChild;
+ // ✅
 
   if (document.querySelector("#game.over")) {
     return;
-  }
+  } // ✅
 
-  console.log(key, expected);
+  console.log({key, expected}); // ✅
 
   if (!window.timer && isLetter) {
     window.timer = setInterval(() => {
       if (!window.gameStart) {
         window.gameStart = new Date().getTime();
       }
-      const currentTime = new Date().getTime();
+
+      const currentTime = (new Date()).getTime();
       const msPassed = currentTime - window.gameStart;
       const sPassed = Math.round(msPassed / 1000);
-      const sLeft = gameTime / 1000 - sPassed;
+      const sLeft = ((gameTime / 1000) - sPassed);
+
       if (sLeft <= 0) {
         gameOver();
+        return;
       }
       document.getElementById("info").innerHTML = sLeft + "";
     }, 1000);
-  }
+  } // ✅
 
   if (isLetter) {
     if (currentLetter) {
@@ -96,8 +105,13 @@ document.querySelector("#game").addEventListener("keypress", (e) => {
       if (currentLetter.nextSibling) {
         addClass(currentLetter.nextSibling, "current");
       }
+    } else {
+      const incorrectLetter = document.createElement("span");
+      incorrectLetter.innerHTML = key;
+      incorrectLetter.className = "letter incorrect extra";
+      currentWord.appendChild(incorrectLetter);
     }
-  }
+  } // ✅
 
   if (isSpace) {
     if (expected !== " ") {
@@ -107,21 +121,15 @@ document.querySelector("#game").addEventListener("keypress", (e) => {
       letterToInvalidate.forEach((letter) => {
         letter.classList.add("incorrect");
       });
-    } else {
-      const incorrectLetter = document.createElement("span");
-      incorrectLetter.innerHTML = key;
-      incorrectLetter.className = "letter incorrect extra";
-      currentWord.appendChild(incorrectLetter);
     }
-
-    currentWord.classList.remove("current");
-    currentWord.nextElementSibling?.classList.add("current");
+    removeClass(currentWord, 'current');
+    addClass(currentWord.nextSibling, 'current');
 
     if (currentLetter) {
       removeClass(currentLetter, "current");
     }
     addClass(currentWord.nextSibling.firstChild, "current");
-  }
+  } // ✅
 
   if (isBackSpace) {
     if (currentLetter && isFirstLetter) {
@@ -146,25 +154,29 @@ document.querySelector("#game").addEventListener("keypress", (e) => {
       removeClass(currentWord.lastChild, "incorrect");
       removeClass(currentWord.lastChild, "correct");
     }
-  }
+  } // ✅
 
   //move lines
   if (currentWord.getBoundingClientRect().top > 250) {
     const words = document.getElementById("words");
     const margin = parseInt(words.style.marginTop || "0px");
-    words.style.marginTop = margin - 35 + "px";
-  }
+    words.style.marginTop = (margin - 35) + "px";
+  } // ✅
 
   // move cursor
   const nextLetter = document.querySelector(".letter.current");
   const nextWord = document.querySelector(".word.current");
   const cursor = document.querySelector("#cursor");
-  cursor.style.top =
-    (nextLetter || nextWord).getBoundingClientRect().top + 2 + "px";
+  cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + "px";
   cursor.style.left =
     (nextLetter || nextWord).getBoundingClientRect()[
       nextLetter ? "left" : "right"
     ] + "px";
+});
+
+document.getElementById('refresh').addEventListener('click',()=>{
+  gameOver();
+  newGame();
 });
 
 newGame();
